48dbcb5a5b730c2131d87a14afc56bd05befe6dc
AlexandreC93 <alexandrecadinot93@gmail.com>
2025-01-07 19:18:51 +0100

    all files

--- srcs/requirements/mariadb/tools/mariadb_script.sh
+++ srcs/requirements/mariadb/tools/mariadb_script.sh
@@ -7,7 +7,7 @@ mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
 
 # Créer l'utilisateur s'il n'existe pas
 mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
-
+echo ${SQL_ROOT_PASSWORD}
 # Accorder tous les privilèges à l'utilisateur sur la base de données
 mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 
