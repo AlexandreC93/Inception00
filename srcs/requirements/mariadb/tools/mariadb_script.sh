324a29249a813a4dc1280579cb4ce8734f0b4681
AlexandreC93 <alexandrecadinot93@gmail.com>
2025-01-07 19:44:33 +0100

    all files

--- srcs/requirements/mariadb/tools/mariadb_script.sh
+++ srcs/requirements/mariadb/tools/mariadb_script.sh
@@ -2,10 +2,10 @@
 # Démarrer le service MariaDB
 service mariadb start;
 
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 # Créer la base de données si elle n'existe pas
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
 
-#mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 echo ${SQL_ROOT_PASSWORD}
 # Créer l'utilisateur s'il n'existe pas
 mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
