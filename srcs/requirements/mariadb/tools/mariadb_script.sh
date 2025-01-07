bfcd761d9930a4381322843034cce3196f55bc0b
AlexandreC93 <alexandrecadinot93@gmail.com>
2025-01-07 19:55:33 +0100

    all files

--- srcs/requirements/mariadb/tools/mariadb_script.sh
+++ srcs/requirements/mariadb/tools/mariadb_script.sh
@@ -3,7 +3,6 @@
 service mariadb start;
 
 echo ${SQL_ROOT_PASSWORD}
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 # Créer la base de données si elle n'existe pas
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
 
@@ -13,6 +12,7 @@ mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '$
 mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 
 # Changer le mot de passe du root
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 
 # Reactualisation des privilèges
 mysql -e "FLUSH PRIVILEGES;"
