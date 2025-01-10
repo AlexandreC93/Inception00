ccb5d8cd98cd0943902ef3d95895636ce4c7bf33
AlexandreC93 <alexandrecadinot93@gmail.com>
2025-01-10 17:29:02 +0100

    all files

--- srcs/requirements/mariadb/tools/mariadb_script.sh
+++ srcs/requirements/mariadb/tools/mariadb_script.sh
@@ -1,25 +1,33 @@
 #!/bin/bash
-# Démarrer le service MariaDB
-service mariadb start;
 
-echo ${SQL_ROOT_PASSWORD}
+# Démarrer le service MariaDB
+service mariadb start
+
+# Attendre que MariaDB soit prêt
+until mysqladmin ping > /dev/null 2>&1; do
+  echo "Waiting for MariaDB to be ready..."
+  sleep 2
+done
+
+echo "MariaDB is ready."
+
 # Créer la base de données si elle n'existe pas
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+mysql -uroot -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
 
 # Créer l'utilisateur s'il n'existe pas
-mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -uroot -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 
 # Accorder tous les privilèges à l'utilisateur sur la base de données
-mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%';"
 
-# Changer le mot de passe du root
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+# Définir le mot de passe pour l'utilisateur root
+mysql -uroot -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 
 # Reactualisation des privilèges
-mysql -e "FLUSH PRIVILEGES;"
+mysql -uroot -p${SQL_ROOT_PASSWORD} -e "FLUSH PRIVILEGES;"
 
 # Arrêter proprement MariaDB
-mysqladmin -u root -p${SQL_ROOT_PASSWORD} shutdown
+mysqladmin -uroot -p${SQL_ROOT_PASSWORD} shutdown
 
 # Lancer MariaDB en mode sécurisé
 exec mysqld_safe
