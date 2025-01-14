2ae4688bce9b582901938a4544363bd5992a91e2
AlexandreC93 <alexandrecadinot93@gmail.com>
2025-01-14 14:55:11 +0100

    all files

--- srcs/requirements/mariadb/tools/mariadb_script.sh
+++ srcs/requirements/mariadb/tools/mariadb_script.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 # Démarrer le service MariaDB
 echo "test"
+
 service mariadb start;
+echo "Waiting for MariaDB to be ready..."
+until mysqladmin ping -hlocalhost --silent; do
+  echo "MariaDB is not ready. Retrying..."
+  sleep 2
+done
+echo "MariaDB is ready!"
+
+# Changer le mot de passe du root
+mysql -uroot -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 
 # Créer la base de données si elle n'existe pas
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
@@ -11,9 +21,6 @@ mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '$
 
 # Accorder tous les privilèges à l'utilisateur sur la base de données
 mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-
-# Changer le mot de passe du root
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 
 # Reactualisation des privilèges
 mysql -e "FLUSH PRIVILEGES;"
