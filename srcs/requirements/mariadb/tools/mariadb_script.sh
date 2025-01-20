7f8c75488329ab3af2f7f25d24aad27a27293e87
Laurent-alexandre CADINOT <lcadinot@bess-f2r3s1.clusters.42paris.fr>
2025-01-20 14:15:48 +0100

    fix

--- srcs/requirements/mariadb/tools/mariadb_script.sh
+++ srcs/requirements/mariadb/tools/mariadb_script.sh
@@ -3,7 +3,7 @@ echo "test";
 
 service mariadb start;
 
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+#mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
 
 #mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
 
