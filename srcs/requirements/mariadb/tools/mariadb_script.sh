33b05052b06054c9e9976f52ad3018c9ccc98c49
Laurent-alexandre CADINOT <lcadinot@bess-f2r3s1.clusters.42paris.fr>
2025-01-20 09:28:56 +0100

    fix

--- srcs/requirements/mariadb/tools/mariadb_script.sh
+++ srcs/requirements/mariadb/tools/mariadb_script.sh
@@ -3,13 +3,13 @@ echo "test"
 
 service mariadb start;
 
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\` IDENTIFIED BY '${SQL_PASSWORD}';"
+
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 
 mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
 
 mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 
 mysql -e "FLUSH PRIVILEGES IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 
